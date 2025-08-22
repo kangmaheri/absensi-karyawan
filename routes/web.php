6999dfb18edae4ae6265a8c5ad0f70b5cb954f73
kangmaheri <kangmaheri@gmail.com>
2025-08-22 19:47:32 +0700

    Menambah EmployeeController dan AttendanceController resource. Menambah route resource untuk employees dan attendances di web.php.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\AttendanceController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +12,9 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('employees', EmployeeController::class);
+Route::resource('attendances', AttendanceController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
